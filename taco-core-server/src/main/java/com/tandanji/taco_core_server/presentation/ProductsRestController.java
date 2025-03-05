b5ca7355912b15df04fd107acff8ffaa9805573d
Jung Youngmok <dudahr159@gmail.com>
2025-03-05 21:30:13 +0900

    Initialize project configuration

---
+++ taco-core-server/src/main/java/com/tandanji/taco_core_server/presentation/ProductsRestController.java
@@ -0,0 +1,14 @@
+package com.tandanji.taco_core_server.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@RestController
+public class ProductsRestController {
+
+
+}
---
+++ taco-core-server/src/test/java/com/tandanji/taco_core_server/TacoCoreServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tandanji.taco_core_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCoreServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

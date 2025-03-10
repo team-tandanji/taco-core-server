181721849dc1ec05d7e95620cbb498962292e0e6
Jung Youngmok <dudahr159@gmail.com>
2025-03-10 22:05:44 +0900

    change: move save image function to service layer

--- taco-core-server/src/main/java/com/tandanji/taco_core_server/application/ProductsService.java
+++ taco-core-server/src/main/java/com/tandanji/taco_core_server/application/ProductsService.java
@@ -4,6 +4,12 @@ import com.tandanji.taco_core_server.domain.Product;
 import com.tandanji.taco_core_server.infrastructure.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class ProductsService {
@@ -17,15 +23,29 @@ public class ProductsService {
         this.validationService = validationService;
     }
 
-    //TODO: Valid check about product
-    public Product createProduct(Product product) {
+    public void createProduct(Product product, MultipartFile image) throws IOException {
         validationService.checkValid(product);
-        int result = productRepository.createProduct(product);
 
-        if(result == 1) {
-            return product;
+        product = saveImage(product, image);
+
+        productRepository.createProduct(product);
+    }
+
+    public static Product saveImage(Product product, MultipartFile image) throws IOException {
+        if (image != null) {
+            String imageSaveDir = System.getProperty("user.dir") + "/image";
+
+            String imageFileName = System.currentTimeMillis() + "-" + image.getOriginalFilename();
+            Path imagePath = Paths.get(imageSaveDir, imageFileName);
+
+            Files.createDirectories(imagePath.getParent());
+            image.transferTo(imagePath.toFile());
+
+            product.setImagePath(imagePath.toString());
         } else {
-            return null;
+            product.setImagePath(null);
         }
+
+        return product;
     }
 }
--- taco-core-server/src/main/java/com/tandanji/taco_core_server/presentation/ProductController.java
+++ taco-core-server/src/main/java/com/tandanji/taco_core_server/presentation/ProductController.java
@@ -1,10 +1,7 @@
 package com.tandanji.taco_core_server.presentation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tandanji.taco_core_server.application.ProductsService;
 import com.tandanji.taco_core_server.domain.Product;
-import jakarta.validation.Valid;
-import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 public class ProductController {
@@ -27,26 +21,11 @@ public class ProductController {
     }
 
     @PostMapping(value = "/product/create")
-    public ResponseEntity<Void> createProduct(@RequestParam("product") String productJson,
-                                              @RequestParam(value = "image", required = false) MultipartFile image) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Product product = objectMapper.readValue(productJson, Product.class);
+    public ResponseEntity<Void> createProduct(@RequestPart("product") Product product,
+                                              @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
 
-        if(image!=null) {
-            String imageDir = "/Users/jung-youngmok/SwDevlopement/Backend/taco-core-server/image/";
-            String imageFileName = image.getOriginalFilename();
-            Path imagePath = Paths.get(imageDir, imageFileName);
+        productsService.createProduct(product, image);
 
-            Files.createDirectories(imagePath.getParent());
-            image.transferTo(imagePath.toFile());
-
-            product.setImagePath(imagePath.toString());
-        } else {
-            product.setImagePath(null);
-        }
-
-
-        productsService.createProduct(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
@@ -55,11 +34,13 @@ public class ProductController {
     public ResponseEntity<?> getProducts() {
         return ResponseEntity.ok().body(null);
     }
+
     //TODO: make a put method
     @PutMapping("/product/update/{id}")
     public ResponseEntity<?> updateProduct() {
         return ResponseEntity.ok().body(null);
     }
+
     //TODO: make a delete method
     @DeleteMapping("/product/delete/{id}")
     public ResponseEntity<?> deleteProduct() {

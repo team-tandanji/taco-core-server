bd2c421c145625b83c1528ca20b47c8eb0e04e80
YmJung <dudahr159@gmail.com>
2025-03-21 20:02:29 +0900

    Add: add access log

--- taco-core-server/src/main/java/com/tandanji/taco_core_server/infrastructure/MDCFilter.java
+++ taco-core-server/src/main/java/com/tandanji/taco_core_server/infrastructure/MDCFilter.java
@@ -1,30 +1,53 @@
 package com.tandanji.taco_core_server.infrastructure;
 
-import jakarta.servlet.*;
-import org.apache.logging.log4j.core.config.Order;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.slf4j.MDC;
-import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
-@Order(Ordered.HIGHEST_PRECEDENCE) // 필터 우선순위 설정
-public class MDCFilter implements Filter {
+public class MDCFilter extends OncePerRequestFilter {
+
+    private static final Logger accessLogger = LogManager.getLogger("ACCESS_LOG");
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
+        long startTime = System.currentTimeMillis();
         String requestId = UUID.randomUUID().toString();
-        MDC.put("requestId", requestId); // MDC에 requestId 저장
+        MDC.put("requestId", requestId);
+
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
         try {
-            chain.doFilter(request, response); // 다음 필터로 요청 전달
+            filterChain.doFilter(request, responseWrapper);
         } finally {
-            MDC.clear(); // 응답 후 MDC 정리
+            long duration = System.currentTimeMillis() - startTime;
+            int status = responseWrapper.getStatus();
+
+            accessLogger.info("[{}] {} {} from IP: {} - {}ms - Status: {}",
+                    requestId,
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    request.getRemoteAddr(),
+                    duration,
+                    status
+            );
+
+            MDC.clear();
+            responseWrapper.copyBodyToResponse();
         }
     }
 }
+
 
